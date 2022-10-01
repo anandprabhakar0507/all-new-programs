abb8227c6de004489e088cfc9aff86c1b7fc5af1
Restyled.io <commits@restyled.io>
2022-10-01 04:13:43 +0000

    Restyled by clang-format

--- calculator.java
+++ calculator.java
@@ -1,50 +1,46 @@
 import java.util.*;
-class add{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        Scanner y = new Scanner(System.in);
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = y.nextDouble();
-        System.out.println( + a + " + " + b + " is " + (a + b));
-    }
-   
+class add {
+  public static void main(String args[]) {
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+    double a = x.nextDouble();
+    Scanner y = new Scanner(System.in);
+    System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+    double b = y.nextDouble();
+    System.out.println(+a + " + " + b + " is " + (a + b));
+  }
 }
-class subtract{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println( + a + " - " + b + " is " + (a - b));
-    }
-   
+class subtract {
+  public static void main(String args[]) {
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+    double a = x.nextDouble();
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+    double b = x.nextDouble();
+    System.out.println(+a + " - " + b + " is " + (a - b));
+  }
 }
 class multiplication {
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println( + a + " X " + b + " is  " + (a * b));
-    }
-   
+  public static void main(String args[]) {
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+    double a = x.nextDouble();
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+    double b = x.nextDouble();
+    System.out.println(+a + " X " + b + " is  " + (a * b));
+  }
 }
-class division{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println("quotient of " + a + " / " + b + " is  " + (a / b)+ " and remainder is " +(a % b));
-    }
-   
+class division {
+  public static void main(String args[]) {
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+    double a = x.nextDouble();
+    Scanner x = new Scanner(System.in);
+    System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+    double b = x.nextDouble();
+    System.out.println("quotient of " + a + " / " + b + " is  " + (a / b) +
+                       " and remainder is " + (a % b));
+  }
 }
-
