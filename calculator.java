f90092d89f10d28e19486b85543cb9af6803348b
Anand prabhakar <anandprabhakar0507@gmail.com>
2022-10-01 09:40:45 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class add{
+    public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
+        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+        double a = x.nextDouble();
+        Scanner y = new Scanner(System.in);
+        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+        double b = y.nextDouble();
+        System.out.println( + a + " + " + b + " is " + (a + b));
+    }
+   
+}
+class subtract{
+    public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
+        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+        double a = x.nextDouble();
+        Scanner y = new Scanner(System.in);
+        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+        double b = y.nextDouble();
+        System.out.println( + a + " - " + b + " is " + (a - b));
+    }
+   
+}
+class multiplication {
+    public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
+        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+        double a = x.nextDouble();
+        Scanner y = new Scanner(System.in);
+        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+        double b = y.nextDouble();
+        System.out.println( + a + " X " + b + " is  " + (a * b));
+    }
+   
+}
+class division{
+    public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
+        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
+        double a = x.nextDouble();
+        Scanner y = new Scanner(System.in);
+        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
+        double b = y.nextDouble();
+        System.out.println("quotient of " + a + " / " + b + " is  " + (a / b)+ " and remainder is " +(a % b));
+    }
+   
+}
+
