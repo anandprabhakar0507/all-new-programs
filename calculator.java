6024f29263abc93cdf5c91a414c7ac6e9dabe2f0
Lokesh Katari <lokeshkatari921@outlook.com>
2022-10-02 09:05:26 +0530

    updated calculator using methods

--- calculator.java
+++ calculator.java
@@ -1,55 +1,76 @@
 import java.util.*;
-class add{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println( + a + " + " + b + " is " + (a + b));
+public class calculator{
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    float a,b;
+    
+    while(true){
+        System.out.println("1. Addition");
+        System.out.println("2. subtration");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Exit");
+        System.out.print("Enter Your Choice (1-5): ");
+        int choice=sc.nextInt();
+        switch(choice){
+            case 1:
+            System.out.println("enter the first number");
+             a=sc.nextFloat();
+            System.out.println("enter the second number");
+             b=sc.nextFloat();
+            System.out.println(addition(a,b));
+             break;
+            case 2:
+            System.out.println("enter the first number");
+             a=sc.nextFloat();
+            System.out.println("enter the second number");
+             b=sc.nextFloat();
+            System.out.println(subtraction(a,b));
+             break;
+            case 3:
+            System.out.println("enter the first number");
+             a=sc.nextFloat();
+            System.out.println("enter the second number");
+             b=sc.nextFloat();
+            System.out.println(multiplication(a,b));
+            break;
+            case 4:
+            System.out.println("enter the first number");
+             a=sc.nextFloat();
+            System.out.println("enter the second number");
+             b=sc.nextFloat();
+            System.out.println(division(a,b));
+            break;
+            case 5:
+            return;
+            default:
+            System.out.println("enter valid choice");
+            break;
+
+        }
     }
+
+    //System.out.println(addition(a,b));
    
+
 }
-class subtract{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println( + a + " - " + b + " is " + (a - b));
-    }
-   
+ public static float addition(float a,float b){
+    float sum=a+b;
+    return sum;
+    
 }
-class multiplication {
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println( + a + " X " + b + " is  " + (a * b));
-    }
-   
+public static float subtraction(float a,float b){
+    float sub=a-b;
+    return sub;
 }
-class division{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println("quotient of " + a + " / " + b + " is  " + (a / b)+ " and remainder is " +(a % b));
-    }
-   
+public static float multiplication(float a,float b){
+    float mul=a*b;
+    return mul;
 }
-class modulo{
-    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
-        System.out.println("PLEASE ENTER FIRST NUMBER a: ");
-        double a = x.nextDouble();
-        System.out.println("PLEASE ENTER SECOND NUMBER b: ");
-        double b = x.nextDouble();
-        System.out.println("modulo is " +(a % b));
+public static float division(float a,float b){
+    float div=a/b;
+    return div;
+}
+
 }
 
