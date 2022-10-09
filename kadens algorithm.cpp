dd715fd793161a23e67074e7a4b89e7f57730272
Priyanshu729 <72179746+Priyanshu729@users.noreply.github.com>
2022-10-09 21:12:25 +0530

    Update kadens algorithm.cpp

--- kadens algorithm.cpp
+++ kadens algorithm.cpp
@@ -3,26 +3,26 @@ using namespace std;
 int main()
 {
 	int n;
-	cout<<"ENTER THE SIZE OF ARRAY ";
-	cin>>n;
+	cout<< "ENTER THE SIZE OF ARRAY ";
+	cin >> n;
 	int a[n];
-	for(int i=0;i<n;i++)
+	for(int i=0; i<n; i++)
 	{
-		cin>>a[i];
+		cin >> a[i] ;
 			
-	}cout<<"\n";
+	}cout << "\n" ;
 	
-	int maxSum=INT_MIN;
-    for(int i=0;i<n;i++)
+	int maxSum = INT_MIN;
+    for(int i=0; i<n; i++)
 	{
-		int sum=0;
-		sum+=a[i];
-		if(sum<0)
+		int sum = 0 ;
+		sum + = a[i] ;
+		if(sum < 0)
 		{
-			sum=0;
-		}maxSum=max(maxSum,sum);	
-	}cout<<maxSum<<" ";
-	cout<<"\n";
+			sum = 0;
+		} maxSum= max(maxSum , sum);	
+	}cout<< maxSum <<" " ;
+	cout << "\n";
     
     
     
