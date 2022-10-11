9c3cd4dc2dfcf1474cae252f865dbde8e849005d
Restyled.io <commits@restyled.io>
2022-10-11 19:04:44 +0000

    Restyled by astyle

--- Find Max and Min in Array.c
+++ Find Max and Min in Array.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 int main() {
-  int a[1000], i, n, min, max;
+    int a[1000], i, n, min, max;
 
-  printf("Enter size of the array : ");
-  scanf("%d", &n);
+    printf("Enter size of the array : ");
+    scanf("%d", &n);
 
-  printf("Enter elements in array : ");
-  for (i = 0; i < n; i++) {
-    scanf("%d", &a[i]);
-  }
+    printf("Enter elements in array : ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
 
-  min = max = a[0];
-  for (i = 1; i < n; i++) {
-    if (min > a[i])
-      min = a[i];
-    if (max < a[i])
-      max = a[i];
-  }
-  printf("minimum of array is : %d", min);
-  printf("\nmaximum of array is : %d", max);
+    min = max = a[0];
+    for (i = 1; i < n; i++) {
+        if (min > a[i])
+            min = a[i];
+        if (max < a[i])
+            max = a[i];
+    }
+    printf("minimum of array is : %d", min);
+    printf("\nmaximum of array is : %d", max);
 
-  return 0;
+    return 0;
 }
