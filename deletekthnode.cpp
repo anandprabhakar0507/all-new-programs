1cf7308e4e620df58a3becaa04625989e6ed8813
Priyanshu729 <72179746+Priyanshu729@users.noreply.github.com>
2022-10-11 08:40:39 +0530

    Update deletekthnode.cpp

--- deletekthnode.cpp
+++ deletekthnode.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct node
-{
+struct node{
+
 	public:
 		int data;
-		struct node* next;
-}*first = NULL,*last = NULL;
-void create(int a[],int n)
+		struct node *next;
+} *first = NULL, *last = NULL;
+void create(int a[], int n)
 {
 	first = new node();
 	first->data = a[0];
@@ -14,26 +14,26 @@ void create(int a[],int n)
 	last = first;
 	for(int i = 1; i < n; i++)
 	{
-		node* t = new node();
+		node *t = new node();
 		t->data = a[i];
 		t->next = NULL;
 		last->next = t;
 		last = t;
 	}
 
-}void display(struct node* p)
+} void display(struct node* p)
 {
 	if(p != NULL)
 	{
-		cout << p->data<<"-->";
+		cout << p->data << "-->";
 		display(p->next);
 	}
-}node* delete_end(struct node* p,int pos)
+} node *delete_end(struct node *p, int pos)
 {
 	int i = 0;
-	node* dummy = new node();
+	node *dummy = new node();
 	dummy->next = p;
-	struct node* f; struct node* s;
+	struct node *f; struct node *s;
 	f = s = dummy;
 	while(i != pos)
 	{
@@ -51,12 +51,12 @@ void create(int a[],int n)
 int main()
 {
 	int a[6]={3,2,5,4,1,7};
-	create(a,6);
+	create(a, 6);
 	display(first);
   int n;
   cout << "Enter the nth node you want to delete ";
   cin >> n;
 	cout << endl;
-	struct node* k= delete_end(first,n);
+	struct node *k= delete_end(first, n);
 	display(k);
  }
