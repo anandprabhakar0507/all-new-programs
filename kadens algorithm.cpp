1163baafbb7c1a41b27ede2c1e84f2df6ab06750
Restyled.io <commits@restyled.io>
2022-10-11 09:25:38 +0000

    Restyled by astyle

--- kadens algorithm.cpp
+++ kadens algorithm.cpp
@@ -2,29 +2,31 @@
 using namespace std;
 int main()
 {
-	int n;
-	cout<< "ENTER THE SIZE OF ARRAY ";
-	cin >> n;
-	int a[n];
-	for(int i = 0; i < n; i++)
-	{
-		cin >> a[i] ;
-			
-	}cout << "\n" ;
-	
-	int maxSum = INT_MIN;
+    int n;
+    cout<< "ENTER THE SIZE OF ARRAY ";
+    cin >> n;
+    int a[n];
     for(int i = 0; i < n; i++)
-	{
-		int sum = 0;
-		sum + = a[i];
-		if(sum < 0)
-		{
-			sum = 0;
-		} maxSum= max(maxSum , sum);	
-	}
-	cout<< maxSum <<" ";
-	cout << "\n";
-    
-    
-    
+    {
+        cin >> a[i] ;
+
+    }
+    cout << "\n" ;
+
+    int maxSum = INT_MIN;
+    for(int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        sum + = a[i];
+        if(sum < 0)
+        {
+            sum = 0;
+        }
+        maxSum= max(maxSum, sum);
+    }
+    cout<< maxSum <<" ";
+    cout << "\n";
+
+
+
 }
