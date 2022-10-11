a1b5a647667550ecbf5da2169120860d9189e1e9
Restyled.io <commits@restyled.io>
2022-10-11 19:04:01 +0000

    Restyled by clang-format

--- FibonacciSeriesUsingFunction.c
+++ FibonacciSeriesUsingFunction.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 void fibo(int);
 void main() {
-    int n;
-    printf("Enter a number to generate fibonacci series for first n terms:", n);
-    scanf("%d", &n);
-    fibo(n);
+  int n;
+  printf("Enter a number to generate fibonacci series for first n terms:", n);
+  scanf("%d", &n);
+  fibo(n);
 }
 
 void fibo(int n) {
-    int i, c = 0;
-    int a = 0;
-    int b = 1;
-    printf("\nFibonacci series for %d terms:-\n", n);
-    for (i = 0; i < n; i++) {
-        printf("%d ", c);
-        a = b;
-        b = c;
-        c = a + b;
-    }
+  int i, c = 0;
+  int a = 0;
+  int b = 1;
+  printf("\nFibonacci series for %d terms:-\n", n);
+  for (i = 0; i < n; i++) {
+    printf("%d ", c);
+    a = b;
+    b = c;
+    c = a + b;
+  }
 }
--- assertion-analyser_and_issue_tracker.js
+++ assertion-analyser_and_issue_tracker.js
@@ -28,32 +28,40 @@
 function objParser(str, init) {
   // finds objects, arrays, strings, and function arguments
   // between parens, because they may contain ','
-  let openSym = ["[", "{", '"', "'", "("];
-  let closeSym = ["]", "}", '"', "'", ")"];
+  let openSym = [ "[", "{", '"', "'", "(" ];
+  let closeSym = [ "]", "}", '"', "'", ")" ];
   let type;
   let i;
   for (i = init || 0; i < str.length; i++) {
     type = openSym.indexOf(str[i]);
-    if (type !== -1) break;
+    if (type !== -1)
+      break;
   }
-  if (type === -1) return null;
+  if (type === -1)
+    return null;
   let open = openSym[type];
   let close = closeSym[type];
   let count = 1;
   let k;
   for (k = i + 1; k < str.length; k++) {
     if (open === '"' || open === "'") {
-      if (str[k] === close) count--;
-      if (str[k] === "\\") k++;
+      if (str[k] === close)
+        count--;
+      if (str[k] === "\\")
+        k++;
     } else {
-      if (str[k] === open) count++;
-      if (str[k] === close) count--;
+      if (str[k] === open)
+        count++;
+      if (str[k] === close)
+        count--;
     }
-    if (count === 0) break;
+    if (count === 0)
+      break;
   }
-  if (count !== 0) return null;
+  if (count !== 0)
+    return null;
   let obj = str.slice(i, k + 1);
-  return { start: i, end: k, obj: obj };
+  return {start : i, end : k, obj : obj};
 }
 
 function replacer(str) {
@@ -63,17 +71,17 @@ function replacer(str) {
   let data = [];
   while ((obj = objParser(str))) {
     data[cnt] = obj.obj;
-    str =
-      str.substring(0, obj.start) + "__#" + cnt++ + str.substring(obj.end + 1);
+    str = str.substring(0, obj.start) + "__#" + cnt++ +
+          str.substring(obj.end + 1);
   }
-  return { str: str, dictionary: data };
+  return {str : str, dictionary : data};
 }
 
 function splitter(str) {
   // split on commas, then restore the objects
   let strObj = replacer(str);
   let args = strObj.str.split(",");
-  args = args.map(function (a) {
+  args = args.map(function(a) {
     let m = a.match(/__#(\d+)/);
     while (m) {
       a = a.replace(/__#(\d+)/, strObj.dictionary[m[1]]);
@@ -91,13 +99,13 @@ function assertionAnalyser(body) {
   // // get test function body
   // body = body.match(/\{\s*([\s\S]*)\}\s*$/)[1];
 
-  if (!body) return "invalid assertion";
+  if (!body)
+    return "invalid assertion";
   // replace assertions bodies, so that they cannot
   // contain the word 'assertion'
 
-  let cleanedBody = body.match(
-    /(?:browser\s*\.\s*)?assert\s*\.\s*\w*\([\s\S]*\)/
-  );
+  let cleanedBody =
+      body.match(/(?:browser\s*\.\s*)?assert\s*\.\s*\w*\([\s\S]*\)/);
   if (cleanedBody && Array.isArray(cleanedBody)) {
     body = cleanedBody[0];
   } else {
@@ -111,26 +119,22 @@ function assertionAnalyser(body) {
   // match the METHODS
 
   let assertionBodies = [];
-  let methods = assertions.map(function (a, i) {
+  let methods = assertions.map(function(a, i) {
     let m = a.match(/^\s*\.\s*(\w+)__#(\d+)/);
     assertionBodies.push(parseInt(m[2]));
     let pre = splittedAssertions[i].match(/browser\s*\.\s*/) ? "browser." : "";
     return pre + m[1];
   });
-  if (
-    methods.some(function (m) {
-      return !m;
-    })
-  )
+  if (methods.some(function(m) { return !m; }))
     return "invalid assertion";
   // remove parens from the assertions bodies
-  let bodies = assertionBodies.map(function (b) {
-    return s.dictionary[b].slice(1, -1).trim();
-  });
-  assertions = methods.map(function (m, i) {
+  let bodies = assertionBodies.map(function(
+      b) { return s.dictionary[b].slice(1, -1).trim(); });
+  assertions = methods.map(function(m, i) {
     return {
-      method: m,
-      args: splitter(bodies[i]), // replace objects, split on ',' ,then restore objects
+      method : m,
+      args : splitter(
+          bodies[i]), // replace objects, split on ',' ,then restore objects
     };
   });
   return assertions;
