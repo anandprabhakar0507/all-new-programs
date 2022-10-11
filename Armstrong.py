3e2e2810ead3c4f543d7efd79ebf449156add363
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2022-10-12 00:59:13 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -54,3 +54,23 @@ if num == sum:
     print(num, "is an Armstrong number")
 else:
     print(num, "is not an Armstrong number")
+#3rd method
+#Contributed by: Sirsha 
+#Armstrong number using recursive approach
+number = int(input("Enter a number: "))
+#Taking input from user
+order = len(str(number))
+sum = 0
+#Assigning the value of sum = 0
+temp = number
+
+#going through every digit of number store in temp variable
+while temp > 0:
+    n = temp % 10
+    temp = temp // 10
+#Storing the sum of cube of the digits in sum variable
+    sum = sum + pow(n, order)
+if(sum == number):
+    print(number," is an armstrong number")
+else:
+    print(number," is not an armstrong number")
