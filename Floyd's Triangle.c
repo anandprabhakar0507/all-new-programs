1a3bc1f27c44bab0aaeeed45037ad809e86e54a4
Restyled.io <commits@restyled.io>
2022-10-11 19:03:40 +0000

    Restyled by astyle

--- Floyd's Triangle.c
+++ Floyd's Triangle.c
@@ -7,14 +7,14 @@ int main()
     printf("Enter the no. of rows:");
     scanf("%d",&n);
 
-    for(int i=1;i<=n;i++)
+    for(int i=1; i<=n; i++)
     {
-        for(int j=1;j<i;j++)
+        for(int j=1; j<i; j++)
         {
             printf("%d ",k);
             k=k+1;
         }
-    printf("\n");
+        printf("\n");
     }
     return 0;
 }
